082ef788dc957cf01a439e8ca8c8a9537efa392a
Konrad <54865575+KonradDanielewski@users.noreply.github.com>
2025-01-16 10:34:47 +0100

    cleanup and docstrings

--- deepecohab/social_structure.py
+++ deepecohab/social_structure.py
@@ -8,7 +8,7 @@ import toml
 from deepecohab.plotting import _plot_weighted_ranking
 
 
-def graph_distances(graph):
+def _graph_distances(graph) -> od.Series:
     """Auxfun to get distance from the center fro the network graph
     """    
     distances = [np.linalg.norm(graph[animal] - [0,0]) for animal in graph.keys()]
@@ -19,16 +19,25 @@ def graph_distances(graph):
 
     return distances
 
-def weigh_ranking(cfp: str, graph: dict, ranking_ordinal: pd.Series, chasings: pd.DataFrame, plot: bool = True, save_plot: bool = False) -> pd.Series:
-    """_summary_
+def weigh_ranking(
+    cfp: str, 
+    graph: dict, 
+    ranking_ordinal: pd.Series, 
+    chasings: pd.DataFrame, 
+    plot: bool = True, 
+    save_plot: bool = True, 
+    show_plot: bool = True,
+) -> pd.Series:
+    """Creates a weighted ranking based on multiple features of the social structure.
 
     Args:
-        cfp: _description_
-        graph: _description_
-        ranking: _description_
-        chasings: _description_
-        plot: _description_. Defaults to True.
-        save_plot: _description_. Defaults to False.
+        cfp: path to project config file
+        graph: node location from the graph network
+        ranking_ordinal: ranking created during chasings calculation
+        chasings: chasings matrix created with calculate_chasings
+        plot: toggle whether to create the plot. Defaults to True.
+        save_plot: toggle whether to save the plot. Defaults to True.
+        show_plot: toggle whether to save the plot. Defaults to True.
 
     Returns:
         _description_
@@ -36,7 +45,7 @@ def weigh_ranking(cfp: str, graph: dict, ranking_ordinal: pd.Series, chasings: p
     cfg = toml.load(cfp)
     project_location = Path(cfg["project_location"])
     
-    distances = graph_distances(graph)
+    distances = _graph_distances(graph)
 
     normalized_ranking = ranking_ordinal.copy()
     normalized_ranking.loc[:] = (ranking_ordinal.values - ranking_ordinal.min()) / (ranking_ordinal.max() - ranking_ordinal.min())
@@ -51,6 +60,6 @@ def weigh_ranking(cfp: str, graph: dict, ranking_ordinal: pd.Series, chasings: p
 
     # NOTE: Move this part to the summary plot. In workflow summary plot should be the last one, after ranking in time and network
     if plot:
-        _plot_weighted_ranking(data_prep, project_location, save_plot)
+        _plot_weighted_ranking(data_prep, project_location, save_plot, show_plot)
 
     return data_prep.final_ranking
