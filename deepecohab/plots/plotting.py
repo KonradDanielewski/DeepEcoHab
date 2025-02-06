63cf25f442a77490aa112e4102b2cfdedc555f23
KonradDanielewski <54865575+KonradDanielewski@users.noreply.github.com>
2025-02-06 21:36:28 +0100

    deprecate old plots

--- deepecohab/plots/plotting.py
+++ deepecohab/plots/plotting.py
@@ -10,34 +10,6 @@ from plotly.subplots import make_subplots
 from deepecohab.utils import auxfun
 from deepecohab.utils import auxfun_plots
 
-
-
-def _plot_chasings_matrix(chasings: pd.DataFrame, project_location: Path, save_plot: bool, show_plot: bool):
-    """Auxfun for plotting the chasings matrix
-    """            
-    fig = px.imshow(chasings, text_auto=True, width=700, height=700)
-    fig.update_xaxes(side="top")
-    
-    if save_plot:
-        save_path = project_location / "plots"
-        fig.write_html(save_path / "chasings_matrix.html")
-        # fig.write_image(save_path / "chasings_matrix.svg")
-    if show_plot:
-        fig.show()
-
-def _plot_weighted_ranking(data_prep: pd.DataFrame, project_location: Path, save_plot: bool, show_plot: bool):
-    """Auxfun to plot a barplot with weighted ranking
-    """            
-    fig = px.bar(data_prep["final_ranking"], width=800, height=500)
-    fig.update_yaxes(title="Weighted rank")
-    
-    if save_plot:
-        plot_location = project_location / "plots"
-        fig.write_html(plot_location / r"weighted_ranking.html")
-         # fig.write_image(save_path / "weighted_ranking.svg")
-    if show_plot:
-        fig.show()
-
 def social_dominance_evaluation(
     cfp: str,
     chasings: pd.DataFrame,
