4cb42af0a7a966a2e36d6130eddd8ae17611764c
KonradDanielewski <54865575+KonradDanielewski@users.noreply.github.com>
2025-12-20 14:40:14 +0100

    simplify and optimize ranking calculation

--- deepecohab/antenna_analysis/chasings.py
+++ deepecohab/antenna_analysis/chasings.py
@@ -6,81 +6,71 @@ from openskill.models import PlackettLuce
 from deepecohab.utils import auxfun
 
 
-def _combine_matches(cfg: dict) -> tuple[list, pl.Series]:
-    """Auxfun to combine all the chasing events into one data structure"""
-    match_lf = auxfun.load_ecohab_data(cfg, "match_df")
-    match_df = match_lf.collect().sort('datetime')
-
-    datetimes = match_df["datetime"]
-
-    match_df = match_df.drop("datetime")
-
-    matches = [tuple(row) for row in match_df.iter_rows()]
-
-    return matches, datetimes
-
-
-def _rank_mice_openskill(
-    cfg: dict,
-    animal_ids: list[str],
+def calculate_ranking(
+    config_path: str | Path | dict,
+    overwrite: bool = False,
+    save_data: bool = True,
     ranking: dict | None = None,
-) -> tuple[dict, pl.DataFrame]:
-    """Rank mice using PlackettLuce algorithm from openskill. More info: https://arxiv.org/pdf/2401.05451
-
+) -> pl.LazyFrame:
+    """Calculate ranking using Plackett Luce algortihm. Each chasing event is a match
+        TODO: handling of previous rankings
     Args:
-        cfg: config dict
-        matches: list of all matches structured
-        animal_ids: list of animal IDs
-        ranking: dictionary that contains ranking of animals - if provided the ranking will start from this state. Defaults to None.
+        config_path: path to project config file.
+        save_data: toogles whether to save data.
+        overwrite: toggles whether to overwrite the data.
+        ranking: optionally, user can pass a dictionary from a different recording of same animals
+                 to start ranking from a certain point instead of 0
 
     Returns:
-        ranking: dictionary of the ranking per animal
-        ranking_in_time: pd.DataFrame of the ranking where each row is another match, sorted in time
-        datetimes: pd.Series of all matches datetimes for sorting or axis setting purposes
+        LazyFrame of ranking
     """
+    cfg = auxfun.read_config(config_path)
+    results_path = Path(cfg["project_location"]) / "results"
+    key = "ranking"
+
+    ranking = None if overwrite else auxfun.load_ecohab_data(config_path, key)
+
+    if isinstance(ranking, pl.LazyFrame):
+        return ranking
+    
+    match_df = auxfun.load_ecohab_data(cfg, 'match_df').sort('datetime').collect()
+    animal_ids = cfg['animal_ids']
+
     model = PlackettLuce(limit_sigma=True, balance=True)
-    match_list, datetimes = _combine_matches(cfg)
+    ranking = {player: model.rating() for player in animal_ids}
 
-    ranking = {}
+    rows = []
 
-    for player in animal_ids:
-        ranking[player] = model.rating()
-
-    ranking_df = []
-
-    for (loser_name, winner_name), datetime in zip(match_list, datetimes):
+    for loser_name, winner_name, dtime in match_df.iter_rows():
         new_ratings = model.rate(
-            [[ranking[loser_name]], [ranking[winner_name]]], ranks=[1, 0]
+            [[ranking[loser_name]], [ranking[winner_name]]],
+            ranks=[1, 0],
         )
 
         ranking[loser_name] = new_ratings[0][0]
         ranking[winner_name] = new_ratings[1][0]
 
-        intermediate = {
-                'mu': [],
-                'sigma': [],
-                'ordinal': [],
-                'animal_id': [],
-                'datetime': [],
-            }
-        
-        for animal in ranking.keys():
-            intermediate['mu'].append(ranking[animal].mu)
-            intermediate['sigma'].append(ranking[animal].sigma)
-            intermediate['ordinal'].append(round(ranking[animal].ordinal(), 3))
-            intermediate['animal_id'].append(animal)
-            intermediate['datetime'].append(datetime)
+        for animal, rating in ranking.items():
+            rows.append({
+                "animal_id": animal,
+                "mu": rating.mu,
+                "sigma": rating.sigma,
+                "ordinal": round(rating.ordinal(), 3),
+                "datetime": dtime,
+            })
 
+    ranking_df = pl.LazyFrame(rows).with_columns(
+        auxfun.get_phase(cfg),
+        auxfun.get_day(),
+        auxfun.get_hour(),
+    )
 
-        ranking_df.append(pl.LazyFrame(intermediate))
-    ranking_df = pl.concat(ranking_df)
-    ranking_df = ranking_df.with_columns(
-            auxfun.get_phase(cfg),
-            auxfun.get_day(), 
-            auxfun.get_hour()
+    if save_data:
+        ranking_df.sink_parquet(
+            results_path / "ranking.parquet", compression="lz4", engine='streaming'
         )
 
-    return ranking_df
+    return ranking
 
 
 def calculate_chasings(
@@ -155,42 +145,3 @@ def calculate_chasings(
         matches.sink_parquet(results_path / "match_df.parquet", compression="lz4", engine='streaming')
 
     return chasings
-
-
-def calculate_ranking(
-    config_path: str | Path | dict,
-    overwrite: bool = False,
-    save_data: bool = True,
-    ranking: dict | None = None,
-) -> pl.LazyFrame:
-    """Calculate ranking using Plackett Luce algortihm. Each chasing event is a match
-        TODO: handling of previous rankings
-    Args:
-        config_path: path to project config file.
-        save_data: toogles whether to save data.
-        overwrite: toggles whether to overwrite the data.
-        ranking: optionally, user can pass a dictionary from a different recording of same animals
-                 to start ranking from a certain point instead of 0
-
-    Returns:
-        LazyFrame of ranking
-    """
-    cfg = auxfun.read_config(config_path)
-    results_path = Path(cfg["project_location"]) / "results"
-    key = "ranking"
-
-    ranking = None if overwrite else auxfun.load_ecohab_data(config_path, key)
-
-    if isinstance(ranking, pl.LazyFrame):
-        return ranking
-
-    animals = cfg["animal_ids"]
-
-    ranking = _rank_mice_openskill(cfg, animals, ranking)
-
-    if save_data:
-        ranking.sink_parquet(
-            results_path / "ranking.parquet", compression="lz4", engine='streaming'
-        )
-
-    return ranking
