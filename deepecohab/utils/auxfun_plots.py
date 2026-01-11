add empty joins to fix incohort calculation

deepecohab/utils/auxfun_plots.py
@@ -472,21 +472,6 @@ def prep_activity(
 	positions: list[str],
 ) -> pl.DataFrame:
 	"""Aggregate visits and time spent per position, animal, and day."""
-	join_df = pl.LazyFrame(
-		(
-			product(
-				animals,
-				list(range(days_range[0], days_range[1] + 1)),
-				positions,
-			)
-		),
-		schema=[
-			("animal_id", pl.Enum(animals)),
-			("day", pl.Int16()),
-			("position", pl.String),
-		],
-	)
-
 	df = (
 		store["activity_df"]
 		.lazy()
@@ -496,11 +481,6 @@ def prep_activity(
 		.agg(
 			pl.sum("visits_to_position").alias("visits"),
 			pl.sum("time_in_position").alias("time"),
-		)
-		.join(
-			join_df,
-			on=["animal_id", "position", "day"],
-			how="right",
 		)
 		.sort(["animal_id", "position"])
 		.fill_null(0)
@@ -623,7 +603,7 @@ def prep_pairwise_sociability(
 ) -> np.ndarray:
 	"""Generate a pivot table of pairwise encounters or shared time between animals per location."""
 	join_df = pl.LazyFrame(
-		(product(cages, animals, animals)),
+		product(cages, animals, animals),
 		schema=[
 			("position", pl.Categorical()),
 			("animal_id", pl.Enum(animals)),
@@ -641,7 +621,7 @@ def prep_pairwise_sociability(
 		)
 		.group_by(["animal_id", "animal_id_2", "position"], maintain_order=True)
 		.agg(
-			pl.when(pl.len() > 0).then(pl.sum(pairwise_switch)).alias("sum"),
+			pl.sum(pairwise_switch).alias("sum"),
 			pl.mean(pairwise_switch).alias("mean"),
 		)
 		.join(
@@ -666,10 +646,11 @@ def prep_within_cohort_sociability(
 	phase_type: list[str],
 	animals: list[str],
 	days_range: list[int, int],
+	sociability_switch: Literal["raw", "normalized"] = "sociability",
 ) -> np.ndarray:
 	"""Calculate and pivot the mean sociability scores between all animal pairs within a cohort."""
 	join_df = pl.LazyFrame(
-		(product(animals, animals)),
+		product(animals, animals),
 		schema=[
 			("animal_id", pl.Enum(animals)),
 			("animal_id_2", pl.Enum(animals)),
@@ -678,13 +659,13 @@ def prep_within_cohort_sociability(
 	img = (
 		store["incohort_sociability"]
 		.lazy()
-		.with_columns(pl.col("sociability").round(3))
+		.with_columns(pl.col(sociability_switch).round(3))
 		.filter(
 			pl.col("phase").is_in(phase_type),
 			pl.col("day").is_between(*days_range),
 		)
 		.group_by(["animal_id", "animal_id_2"], maintain_order=True)
-		.agg(pl.mean("sociability").alias("mean"))
+		.agg(pl.mean(sociability_switch).alias("mean"))
 		.join(
 			join_df,
 			on=["animal_id", "animal_id_2"],